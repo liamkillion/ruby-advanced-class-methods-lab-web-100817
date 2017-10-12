aa80a435dd55433ea3e26e7f458ddcdb801326f5
Liam Killion <liam.killion@gmail.com>
2017-10-12 17:48:56 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,8 +7,65 @@ class Song
     @@all
   end
 
-  def save
-    self.class.all << self
+  # def save
+  #   self.class.all << self
+  # end
+
+  def self.create
+    song = self.new
+    @@all<<song
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    @@all<<song
+    song
+  end
+
+
+  def self.find_by_name(song_name)
+    @@all.find {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)==nil
+      self.create_by_name(song_name)
+    else
+      self.find_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file_name)
+    song = self.new
+    file_name_array = file_name.split(' - ')
+    song.artist_name = file_name_array[0]
+    song.name = file_name_array[1].chomp('.mp3')
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    song = self.new
+    file_name_array = file_name.split(' - ')
+    song.artist_name = file_name_array[0]
+    song.name = file_name_array[1].chomp('.mp3')
+    @@all << song
+  end
+
+  def self.destroy_all
+    @@all = []
   end
 
 end
+true
